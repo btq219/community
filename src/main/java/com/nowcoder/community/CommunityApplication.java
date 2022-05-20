3261598e96d1df85ae1e86d37740f051417021ab
bitianqi <bitianqi1998@163.com>
2022-05-20 16:01:50 +0800

    利用elasticsearch开发社区搜索功能

--- src/main/java/com/nowcoder/community/CommunityApplication.java
+++ src/main/java/com/nowcoder/community/CommunityApplication.java
@@ -3,8 +3,19 @@ package com.nowcoder.community;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import javax.annotation.PostConstruct;
+
+
+
 @SpringBootApplication
 public class  CommunityApplication {
+
+	@PostConstruct //管理bean初始化的
+	public void init(){
+		// 解决Netty启动冲突的问题
+		// Netty4Utils.setAvailableProcessors()
+		System.setProperty("es.set.netty.runtime.available.processors","false");
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(CommunityApplication.class, args);
